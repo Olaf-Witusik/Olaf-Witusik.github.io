d4672e888c61d450a4510a7c4c1470259c4e5ab0
galus-anonimus <olaf.witusik@gmail.com>
2024-07-08 12:39:45 +0200

    first-draft

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const articlesList = document.getElementById('articles-list');
-    fetch('articles.json')
+    fetch('../scripts/articles.json')
         .then(response => response.json())
         .then(data => {
             data.articles.forEach(article => {
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 articleElement.innerHTML = `
                     <h2>${article.title}</h2>
                     <p>${article.description}</p>
-                    <a href="articles/${article.file}" target="_blank">Czytaj więcej</a>
+                    <a href="../articles/${article.file}" target="_blank">Czytaj więcej</a>
                 `;
                 articlesList.appendChild(articleElement);
             });
