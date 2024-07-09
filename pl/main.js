6531c9fe3cebd0fb9bc917bcbb98a1f4fc4557cb
galus-anonimus <olaf.witusik@gmail.com>
2024-07-10 01:31:57 +0200

    better

--- pl/main.js
+++ pl/main.js
@@ -1,3 +1,4 @@
+categories = ['Fizyka','Informatyka','Matematyka','Filozofia Nauki'];
 document.addEventListener('DOMContentLoaded', () => {
     const articlesList = document.getElementById('articles-container');
     fetch('articles/articles.json')
@@ -5,18 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => {
             data.articles.forEach(article => {
                 console.log(1)
-                const articleElement = document.createElement('table');
-                articleElement.classList.add('article')
+                const articleElement = document.createElement('div');
+                articleElement.classList.add('article-box');
                 articleElement.innerHTML = `
-                    <tr>
+                <a class=\"article-link\" href=\"articles/${article.file}\">
+                <table class=\"article cat-${categories.indexOf(article.category)}\">
+                <tbody>
+                <tr>
                     <td>
-                        <p><a class="article-link" href="articles/${article.file}" target="_blank">
-                                <b>${article.title}</b>
-                            </a></p>
-                        <a>${article.category}</a>
+                        <p>
+                            <b>${article.title}</b>
+                        </p>
+                        <p><a>${article.category}</a></p>
                     </td>
-                    <td class="date"><a>${article.date}</a></td>
+                    <td class=\"date\"><a>${article.date}</a></td>
                 </tr>
+                </tbody>
+                </table>
+                </a>
                 `;
                 articlesList.appendChild(articleElement);
             });
